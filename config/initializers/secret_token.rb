08d4971b3fe6b0476f9cfe436e599e7b4b59239b
Ajmal Afif <ajmalafif@gmail.com>
2012-11-14 23:56:40 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PasarApp::Application.config.secret_token = '2230e7cc10a35dd56c9c84c7e4c9867696e013a4f5f500f218375734e1b634366c7eee663797e794a86fc92ece966f61dd70c03969cc28e1d45bfc97c5584e09'
