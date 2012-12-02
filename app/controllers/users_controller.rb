ef89cdbb4e4079d4c0417163692dfdad82e97fa9
Ajmal Afif <ajmalafif@gmail.com>
2012-12-02 23:01:23 +0800

    stuck with following feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @following_relationship = FollowingRelationship.new
+    @store = Store.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   has_many :following_relationships, :foreign_key => :follower_id
   has_many :following, :through => :following_relationships
 
+  def following_relationship_for(other_user)
+    self.following_relationships.find_by_following_id(other_user.id)
+  end
+
 end
